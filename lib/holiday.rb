c19046f14bf83d7894180a1e298cd5865ce26717
cedric e goddard <8ceg6@comcast.net>
2019-10-08 00:27:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
       puts "#{season.to_s.capitalize}:"
     holiday.each do |day, supplies|
-      puts " #{days.to_s.split("_")}"
+      puts " #{days.to_s.split("_").collect {|}}" 
     end
     end
 end
