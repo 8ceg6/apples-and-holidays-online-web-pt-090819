d6997f41ab13b320cad479d7eff91ef03d3ddff1
cedric e goddard <8ceg6@comcast.net>
2019-10-08 00:41:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,8 +75,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-    
-end
+    cookout = []
+    holiday_hash.each do |season, holiday|
+      holiday.each do
+        end
+      end
 
 
 
