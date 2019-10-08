222c7235ca199db83b89fbd77ae9d9d1249450e4
cedric e goddard <8ceg6@comcast.net>
2019-10-08 00:22:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-      puts "  #{season.to_s.capitalize}:"
+      puts "#{season.to_s.capitalize}:"
     holiday.each do |day, supplies|
+      puts " #{days.to_s.capitalize}"
     end
     end
 end
